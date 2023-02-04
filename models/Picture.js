0302d6660cb203bc35715187ce049b03da7c681e
Gabriel Barth <gabriel.barth@sempreceub.com>
2023-02-04 10:33:34 -0300

    First Commit

---
+++ models/Picture.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const PictureSchema = new Schema({
+    name: {type: String, required: true},
+    scr:{type: String, required: true}
+})
+
+
+module. exports = mongoose.model('Picture', PictureSchema)
